762fbbefe677a2abb12d84aeb277b43e403122ba
anishthewizard <chunumunnu@gmail.com>
2022-09-08 21:08:32 -0400

    init

---
+++ src/pages/aboutme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+    render() {
+        return (
+            <p>
+                this the about me page
+            </p>
+        );
+    }
+}
+
+export default AboutMe;
---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+class Blog extends Component {
+    render() {
+        return (
+            <p>
+                this is the home page
+            </p>
+        );
+    }
+}
+
+
+export default Blog;
